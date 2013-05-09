6e211400dd19d2bb20975c7186a7d0a49b51299d
Andreas Kloeckner <inform@tiker.net>
2013-05-09 17:12:16 -0400

    Use Lebesgue constant estimator in plot-nodes example.

--- examples/plot-nodes.py
+++ examples/plot-nodes.py
@@ -2,13 +2,22 @@ import matplotlib.pyplot as pt
 import numpy as np
 
 from modepy.nodes import get_warp_and_blend_nodes
-r, s = get_warp_and_blend_nodes(2, 10, want_boundary_nodes=False)
+n = 15
+unit = get_warp_and_blend_nodes(2, n, want_boundary_nodes=True)
 
-pt.plot(r, s, "o")
+from modepy.tools import estimate_lebesgue_constant
+lebesgue = estimate_lebesgue_constant(n, unit, visualize=True)
 
-from modepy.tools import UNIT_VERTICES
-uv = list(UNIT_VERTICES[2])
+from modepy.tools import unit_to_barycentric, barycentric_to_equilateral
+x, y = barycentric_to_equilateral(unit_to_barycentric(unit))
+
+pt.plot(x, y, "o")
+
+from modepy.tools import EQUILATERAL_VERTICES
+uv = list(EQUILATERAL_VERTICES[2])
 uv.append(uv[0])
 uv = np.array(uv)
 pt.plot(uv[:, 0], uv[:, 1], "")
+
+pt.gca().set_aspect("equal")
 pt.show()
