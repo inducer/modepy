becbb287d3c1697e0cf640c03e1a924e74955525
Andreas Kloeckner <inform@tiker.net>
2013-05-15 15:25:54 -0400

    Advertise Py 3 compatibility.

--- setup.py
+++ setup.py
@@ -33,11 +33,13 @@ def main():
             'License :: OSI Approved :: MIT License',
             'Natural Language :: English',
             'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
             # We use conditional expressions, so 2.5 is the bare minimum.
             'Programming Language :: Python :: 2.5',
             'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
-            # 3.x has not yet been tested.
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
             'Topic :: Scientific/Engineering',
             'Topic :: Scientific/Engineering :: Information Analysis',
             'Topic :: Scientific/Engineering :: Mathematics',
