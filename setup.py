e0cc583c12ba3fee57dd13d6585c30678175fb70
Andreas Kloeckner <inform@tiker.net>
2013-06-10 12:20:33 -0400

    Get rid of ez_setup

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: latin1 -*-
 
-def main():
-    import ez_setup
-    ez_setup.use_setuptools()
 
+def main():
     from setuptools import setup
 
     try:
@@ -15,7 +13,8 @@ def main():
 
     version_dict = {}
     init_filename = "modepy/version.py"
-    exec(compile(open(init_filename, "r").read(), init_filename, "exec"), version_dict)
+    exec(compile(open(init_filename, "r").read(), init_filename, "exec"),
+            version_dict)
 
     setup(name="modepy",
           version=version_dict["VERSION_TEXT"],
@@ -23,35 +22,35 @@ def main():
           long_description=open("README.rst", "rt").read(),
           author="Andreas Kloeckner",
           author_email="inform@tiker.net",
-          license = "MIT",
+          license="MIT",
           url="http://documen.tician.de/modepy",
           classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Other Audience',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            # We use conditional expressions, so 2.5 is the bare minimum.
-            'Programming Language :: Python :: 2.5',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Scientific/Engineering :: Information Analysis',
-            'Topic :: Scientific/Engineering :: Mathematics',
-            'Topic :: Scientific/Engineering :: Visualization',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Utilities',
-            ],
+              'Development Status :: 3 - Alpha',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Other Audience',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'Natural Language :: English',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 3',
+              # We use conditional expressions, so 2.5 is the bare minimum.
+              'Programming Language :: Python :: 2.5',
+              'Programming Language :: Python :: 2.6',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3.2',
+              'Programming Language :: Python :: 3.3',
+              'Topic :: Scientific/Engineering',
+              'Topic :: Scientific/Engineering :: Information Analysis',
+              'Topic :: Scientific/Engineering :: Mathematics',
+              'Topic :: Scientific/Engineering :: Visualization',
+              'Topic :: Software Development :: Libraries',
+              'Topic :: Utilities',
+              ],
 
           packages=[
-                "modepy",
-                "modepy.quadrature",
-                ],
+              "modepy",
+              "modepy.quadrature",
+              ],
           install_requires=[
               "numpy>=1.6",
               "pytools>=2013.1",
@@ -60,8 +59,6 @@ def main():
 
           # 2to3 invocation
           cmdclass={'build_py': build_py})
-
-
 
 
 if __name__ == '__main__':
