50b3464ea4c5069456b940ef36ab5faa86a804a4
Alexandru Fikl <alexfikl@gmail.com>
2024-03-17 11:12:37 +0200

    examples: remove mayavi from plot-nodes

--- examples/plot-nodes.py
+++ examples/plot-nodes.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as pt
 import numpy as np
 
 import modepy as mp
+from modepy.tools import (
+    EQUILATERAL_VERTICES, barycentric_to_equilateral, unit_to_barycentric)
 
 
 dims = 3
@@ -9,19 +11,15 @@ n = 10
 
 unit = mp.warp_and_blend_nodes(dims, n)
 
-if 0:
+if False:
     from modepy.tools import estimate_lebesgue_constant
     lebesgue = estimate_lebesgue_constant(n, unit, visualize=True)
-
-from modepy.tools import barycentric_to_equilateral, unit_to_barycentric
-
 
 equi = barycentric_to_equilateral(unit_to_barycentric(unit))
 
 if dims == 2:
     pt.plot(equi[0], equi[1], "o")
 
-    from modepy.tools import EQUILATERAL_VERTICES
     uv = list(EQUILATERAL_VERTICES[2])
     uv.append(uv[0])
     uv = np.array(uv)
@@ -30,10 +28,8 @@ if dims == 2:
     pt.gca().set_aspect("equal")
     pt.show()
 elif dims == 3:
-    import mayavi.mlab as mlab
-    mlab.points3d(
-            equi[0],
-            equi[1],
-            equi[2])
-    mlab.orientation_axes()
-    mlab.show()
+    ax = pt.subplot(1, 1, 1, projection="3d")
+    ax.plot(equi[0], equi[1], equi[2], "o")
+    pt.show()
+else:
+    raise ValueError(f"Unsupported dimensions: {dims}")
