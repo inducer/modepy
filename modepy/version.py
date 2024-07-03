9c1e5dc061368d61d93cffd592c179a3b96bb3f1
Andreas Kloeckner <inform@tiker.net>
2024-07-03 11:05:07 -0500

    Version-reading: get from importlib

--- modepy/version.py
+++ modepy/version.py
@@ -1,2 +1,9 @@
-VERSION = (2024, 1)
-VERSION_TEXT = ".".join(str(i) for i in VERSION)
+import re
+from importlib import metadata
+
+
+VERSION_TEXT = metadata.version("modepy")
+_match = re.match("^([0-9.]+)([a-z0-9]*?)$", VERSION_TEXT)
+assert _match is not None
+VERSION_STATUS = _match.group(2)
+VERSION = tuple(int(nr) for nr in _match.group(1).split("."))
