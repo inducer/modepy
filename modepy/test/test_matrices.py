461708813ac15635319eba7d35610d2e333462bc
Addison Alvey-Blanco <aja13@illinois.edu>
2025-01-29 20:17:22 -0600

    add back quad_mass_matrix test

--- modepy/test/test_matrices.py
+++ modepy/test/test_matrices.py
@@ -34,6 +34,49 @@ import modepy as mp
 
 
 @pytest.mark.parametrize("shape", [
+                         mp.Simplex(1),
+                         mp.Simplex(2),
+                         mp.Simplex(3),
+                         mp.Hypercube(2),
+                         mp.Hypercube(3),
+                         ])
+@pytest.mark.parametrize("order", [0, 1, 2, 4])
+@pytest.mark.parametrize("nodes_on_bdry", [False, True])
+def test_nodal_mass_matrix_against_quad(
+            shape: mp.Shape, order: int, nodes_on_bdry: bool,
+        ) -> None:
+    space = mp.space_for_shape(shape, order)
+    quad_space = mp.space_for_shape(shape, 2*order)
+    quad = mp.quadrature_for_space(quad_space, shape)
+
+    if isinstance(shape, mp.Hypercube) or shape == mp.Simplex(1):
+        if nodes_on_bdry:
+            nodes = mp.legendre_gauss_lobatto_tensor_product_nodes(
+                shape.dim, order,
+            )
+        else:
+            nodes = mp.legendre_gauss_tensor_product_nodes(shape.dim, order)
+    elif isinstance(shape, mp.Simplex):
+        if nodes_on_bdry:
+            nodes = mp.warp_and_blend_nodes(shape.dim, order)
+        else:
+            nodes = mp.VioreanuRokhlinSimplexQuadrature(order, shape.dim).nodes
+
+    else:
+        raise AssertionError()
+
+    basis = mp.orthonormal_basis_for_space(space, shape)
+
+    quad_mass_mat = mp.nodal_quad_mass_matrix(quad, basis.functions, nodes)
+    vdm_mass_mat = mp.mass_matrix(basis, nodes)
+
+    nodes_to_quad = mp.resampling_matrix(basis.functions, quad.nodes, nodes)
+
+    err = la.norm(quad_mass_mat@nodes_to_quad - vdm_mass_mat)/la.norm(vdm_mass_mat)
+    assert err < 1e-14
+
+
+@pytest.mark.parametrize("shape", [
                          mp.Hypercube(1),
                          mp.Hypercube(2),
                          mp.Hypercube(3),
