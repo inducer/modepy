fce50273c46736727ba1d1bc3e8b94cb01aaaeee
Alexandru Fikl <alexfikl@gmail.com>
2025-07-29 10:54:20 +0300

    feat: use sphinx helper to reroute references

--- doc/conf.py
+++ doc/conf.py
@@ -4,8 +4,7 @@ from importlib import metadata
 from urllib.request import urlopen
 
 
-_conf_url = \
-        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+_conf_url = "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
 with urlopen(_conf_url) as _inf:
     exec(compile(_inf.read(), _conf_url, "exec"), globals())
 
@@ -24,9 +23,21 @@ intersphinx_mapping = {
 
 autodoc_member_order = "bysource"
 
-# FIXME: This would be nice to have, but I was not able to resolve the
-# resulting mess of Sphinx errors.
-#
-autodoc_type_aliases = {
-    # "ArrayF": "modepy.typing.ArrayF",
+sphinxconfig_missing_reference_aliases = {
+    # numpy
+    "NDArray": "obj:numpy.typing.NDArray",
+    "np.floating": "class:numpy.floating",
+    "np.integer": "class:numpy.integer",
+    "np.random.Generator": "class:numpy.random.Generator",
+    "numpy": "mod:numpy",
+    # modepy typing
+    "ArrayF": "obj:modepy.typing.ArrayF",
+    "BasisFunction": "obj:modepy.typing.BasisFunction",
+    "BasisGradient": "obj:modepy.typing.BasisGradient",
+    "NodalFunction": "obj:modepy.typing.NodalFunction",
+    "RealValueT": "obj:modepy.typing.RealValueT",
 }
+
+
+def setup(app):
+    app.connect("missing-reference", process_autodoc_missing_reference)  # noqa: F821
