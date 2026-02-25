74b8d51d7c41bfb67f6d01a52b06dbe75a3e1897
xywei <wxy0516@gmail.com>
2026-02-25 09:40:14 -0600

    fix: test regex, simply kte

--- modepy/__init__.py
+++ modepy/__init__.py
@@ -111,8 +111,6 @@ from modepy.version import VERSION_TEXT as __version__  # noqa: N811
 
 
 GaussLegendreQuadrature = LegendreGaussQuadrature
-Transplanted1DQuadrature = transplanted_1d_quadrature
-TransplantedLegendreGaussQuadrature = transplanted_legendre_gauss_quadrature
 
 __all__ = [
     "PN",
@@ -143,8 +141,6 @@ __all__ = [
     "TensorProductQuadrature",
     "TensorProductShape",
     "TensorProductSpace",
-    "Transplanted1DQuadrature",
-    "TransplantedLegendreGaussQuadrature",
     "VioreanuRokhlinSimplexQuadrature",
     "WitherdenVincentQuadrature",
     "XiaoGimbutasSimplexQuadrature",
